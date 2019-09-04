126f29fd40e6894f9dd2c741fdb1e40c0c432d74
Rebeca Moura <33517058+becahp@users.noreply.github.com>
2019-09-04 08:51:25 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,27 +1,31 @@
 from model_utils import *
 
+#get fixed content from input.inp
 input ='input.inp'
-
 inFile = open(input,"r")
 contents = inFile.read()
 inFile.close()
 
+#expected steps
 steps = [0.5, 0.2, 0.1]
-dv_max = steps[2]
+dv_max = steps[2] #change here
 
+#create directory to store files
 path = os.getcwd()
 directory = join(path, str(dv_max))
 
 if not exists(directory):
     os.makedirs(directory)
 
+#create list of bias values
 list = gen_bias_array(dv_max)
-print(list)
+#print(list)
 
-
+#main logic to create multiple files
 for vs in list:
     for vgb in list:
         for vgs in list:
+            #bias values have to be formatted for constancy
             block_vs = format_number(vs)
             block_vgb = format_number(vgb)
             block_vgs = format_number(vgs)
