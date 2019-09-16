0b0af6cc619acceb02b45be18e0cb2b036bc4ccf
Rebeca Moura <33517058+becahp@users.noreply.github.com>
2019-09-16 11:05:50 -0300

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ inFile.close()
 
 #expected steps
 steps = [0.5, 0.2, 0.1]
-dv_max = steps[2] #change here
+dv_max = steps[1] #change here
 
 #create directory to store files
 path = os.getcwd()
@@ -23,6 +23,12 @@ list = gen_bias_array(dv_max)
 
 #main logic to create multiple files
 for vs in list:
+    #creates new folders to store files when dv_max is 0.1
+    if dv_max == 0.1 :
+        dir = make_new_dir(directory,format_number(vs))
+    else:
+        dir = directory
+        
     for vgb in list:
         for vgs in list:
             #bias values have to be formatted for constancy
@@ -31,5 +37,5 @@ for vs in list:
             block_vgs = format_number(vgs)
 
             block = gen_bias_block(dv_max, block_vs, block_vgb, block_vgs)
-            write_file(directory, contents, dv_max, block_vs, block_vgb, block_vgs)
+            write_file(dir, contents, dv_max, block_vs, block_vgb, block_vgs)
             print(block)
